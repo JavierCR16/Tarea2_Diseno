db86ceb832a82704d0a6a51a0accdfacd4c16999
JavierCR16 <javier.contrerasm16@gmail.com>
2018-02-20 05:57:27 -0600

    dfsd

--- src/Controladores/Main.java
+++ src/Controladores/Main.java
@@ -1,4 +1,4 @@
-package Modelo;
+package Controladores;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../Interfaz/VentanaLogin.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
---
+++ src/Modelo/EstadoTicket.java
@@ -0,0 +1,10 @@
+package Modelo;
+
+/**
+ * Created by Javier on 2/20/2018.
+ */
+public enum EstadoTicket {
+
+    ATENDIDO,ENATENCION,SINATENDER
+
+}
